95784bda066ebdb828acff72a0dd52d55b8a09e5
Drew Payton <drewpayton18@gmail.com>
2022-08-06 14:26:20 -0500

    adding the file structure for the models

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+db = require('./config/connection');
+
 
 const PORT = 3001 || process.env.PORT;
 const app = express();
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`The server has started on http://localhost:${PORT}`))
+})
 
